93bafb7059ec4776cdb9b5124441958c6fdc5510
Ivan Solowjew <ivansolowjew@gmail.com>
2017-03-26 11:54:40 +0300

    Revert: refactoring

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class HomeController extends Controller
+{
+
+    /**
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('home');
+    }
+}
