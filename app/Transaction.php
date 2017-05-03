aeba04fe2f244966a064d7fe2e10504c2429af1e
Ivan Soloviov <Ivan.Soloviov@appfor.de>
2017-05-03 08:52:19 +0300

    #6 - Timestamp timezone. Display transaction timestamps in AEST time

--- app/Transaction.php
+++ app/Transaction.php
@@ -2,14 +2,28 @@
 
 namespace App;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 
 class Transaction extends Model
 {
     protected $guarded = ['id'];
 
+    protected $dates = ['created_at', 'updated_at'];
+
     public function user()
     {
         return $this->belongsTo(\App\User::class);
+    }
+
+    /**
+     * Get created_at date mutator
+     *
+     * @param $value
+     * @return static
+     */
+    public function getCreatedAtAttribute($value)
+    {
+        return Carbon::parse($value)->setTimezone('Australia/Sydney');
     }
 }
