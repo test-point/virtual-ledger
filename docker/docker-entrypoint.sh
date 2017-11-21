8e08cceffbafb2421a3b4257201ba82c0edc3af6
Ivan Soloviov <ivansolowjew@gmail.com>
2017-11-21 20:12:51 +0200

    comment unused command

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -15,7 +15,7 @@ chmod 600 .gnupg/*
 [[ ! -f ".gnupg/gpg-agent.conf" ]] && echo "allow-loopback-pinentry" > .gnupg/gpg-agent.conf
 [[ ! -f ".gnupg/gpg.conf" ]] && echo "pinentry-mode loopback" > .gnupg/gpg.conf
 
-php artisan db:create
+#php artisan db:create
 php artisan --force migrate
 
 exec "${@}"
