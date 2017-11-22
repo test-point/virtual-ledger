c7d4ecee137a2375790976bc6f746cd41e6cb346
Ivan Soloviov <ivansolowjew@gmail.com>
2017-11-22 08:53:20 +0200

    update cronjobs

--- docker/cron.sh
+++ docker/cron.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 source /env
 cd /var/www/html
-php artisan virtual_ledger:load-messages
-php artisan providers:load-transactions
-php artisan providers:load-approved-transactions
-php artisan providers:load-paid-transactions
-php artisan providers:send-messages
+php artisan transactions
+php artisan load-messages
