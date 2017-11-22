c6377033d4aef7710db254cdccf33ddc538a20eb
Ivan Soloviov <ivansolowjew@gmail.com>
2017-11-22 11:09:38 +0200

    debug routes

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,20 @@ Route::group(['middleware' => ['auth']], function () {
 
 Route::get('/social/redirect/{provider}',   ['as' => 'social.redirect',   'uses' => 'Auth\SocialController@getSocialRedirect']);
 Route::get('/oauth_login', 'Auth\SocialController@getSocialHandle');
+
+Route::get('/error-log', function(){
+    if(file_exists(storage_path('logs/laravel.log'))) {
+        $handle = fopen(storage_path('logs/laravel.log'), "r");
+        if ($handle) {
+            while (($line = fgets($handle)) !== false) {
+                echo $line . '<br>';
+            }
+
+            fclose($handle);
+        }
+    }
+});
+
+Route::get('/error-log-clear', function(){
+    return file_put_contents(storage_path('logs/laravel.log'), '');
+});
