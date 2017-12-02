66badba5aac238abfd1b178ab782f1933ebb3293
Ivan Soloviov <ivansolowjew@gmail.com>
2017-12-02 10:57:47 +0200

    send message

--- app/Classes/ApiRequest.php
+++ app/Classes/ApiRequest.php
@@ -86,6 +86,7 @@ class ApiRequest
     public function sendMessage($endpoint, $message, $signature)
     {
         $messageFile = md5($message);
+        error_log('sendMessage: ' . $messageFile);
         $signatureFile = md5($signature);
         Storage::put($messageFile, $message);
         Storage::put($signatureFile, $signature);
